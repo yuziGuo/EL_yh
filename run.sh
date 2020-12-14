592bcb3a8d8ac35bc842d732b88375b41811d78d
yuziGuo <gyh_fish@126.com>
2020-12-14 18:14:01 +0800

    alter shell file

--- run.sh
+++ run.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
-ipython ./cache_kb/cache.py
+python ./cache_kb/cache.py
 python ./evaluation/kb_lookup.py --mode oracle
 python ./evaluation/kb_lookup.py --mode top
+
+python ./cache_kb/cache_extend.py
